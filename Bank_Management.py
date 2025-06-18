7e06b6aacf8024fe47da659faec559e41e1468cd
CHETHAN-GUPTA-ANNAM <annam.chethangupta2024@vitstudent.ac.in>
2025-06-18 09:08:15 +0530

    Update Bank_Management.py

--- Bank_Management.py
+++ Bank_Management.py
@@ -17,7 +17,7 @@ class Bank:
             self.balance=self.balance-ammount
             return 0
     def display(self):
-        print(" ")
+        print("\n\n")
         print(self.name," your account details are as followed: ")
         print(" ")
         print("User Name: ",self.name)
@@ -25,26 +25,26 @@ class Bank:
         print("User Mobile Number: ",self.mobile)
         print("Gender: ",self.gen)
         print("-------------------------")
-        print("User Bank Balance: ",self.balance)
+        print("User Bank Balance: Rs.",self.balance)
         print("-------------------------")
-        print(" ")
+        print(" \n\n")
 
         
 name=str(input("Enter your name: "))
-acc_num=str(input("Enter your account number: "))
-while len(acc_num)!=12:
+acc_num=str(input("Enter your account number(16 DIGIT): "))
+while len(acc_num)!=16:
     print("Wrong account number! Enter again!!")
     print(" ")
-    acc_num=str(input("Enter your account number again: "))
-mobile=str(input("Enter your mobile number: "))
+    acc_num=str(input("Enter your account number again (16 DIGIT): "))
+mobile=str(input("Enter your mobile number(10 DIGIT): "))
 while len(mobile)!=10:
     print("Wrong mobile number! Enter again!!")
     print(" ")
-    mobile=str(input("Enter your mobile number again: "))
-gender=input("Enter M/F/B: ")
-balance=int(input("Enter the balance ammount: "))
+    mobile=str(input("Enter your mobile number again:(10 DIGIT) "))
+gender=input("Enter you gender M/F/B: ")
+balance=int(input("Enter the initial ammount: "))
 b1=Bank(name,acc_num,balance,mobile,gender)
-a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Stop): "))
+a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Exit): "))
 while a!="Stop":
     if a== "Withdraw":
         amm=int(input("Enter the ammount to be withdrawl: "))
@@ -53,16 +53,18 @@ while a!="Stop":
             b1.display()
         else:
             print("Please Try again")
-        a=str(input("Enetr your requirement(Withdraw/Deposit/Check Balance/Stop): "))
+        a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Exit): "))
     elif a=="Deposit":
         amm=int(input("Enter the ammount to be deposited: "))
         b1.deposit(amm)
         b1.display()
-        a=str(input("Enetr your requirement(Withdraw/Deposit/Check Balance/Stop): "))
+        a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Exit): "))
     elif a=="Check Balance":
         b1.display()
-        a=str(input("Enetr your requirement(Withdraw/Deposit/Check Balance/Stop): "))
-    elif a=="Stop":
+        a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Exit): "))
+    elif a=="Exit":
+        print("\nThankyou! Have a nice day ",name)
         break
-    
-        
+    else:
+        print("\n This is case sensitive please enter exact option with correct cases!! ")
+        a=str(input("Enter your requirement(Withdraw/Deposit/Check Balance/Exit): "))
